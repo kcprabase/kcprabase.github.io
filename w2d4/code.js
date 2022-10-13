b88b7c61f8adad1e222291f2910d3611c46dd6e5
Prabesh KC <kcprabase@gmail.com>
2022-10-13 14:55:40 -0500

    updated code.js

--- w2d4/code.js
+++ w2d4/code.js
@@ -22,20 +22,25 @@ Array.prototype.bubbleSort = function () {
 }
 
 //Exercise 3:
-let Person = function (name, age){
+var Person = function(){};
+
+Person.prototype.initialize = function(name, age){
     this.name = name;
     this.age = age;
 };
 
-let Teacher = function(name, age){
-    Person.call(this, name, age);
-};
+var Teacher = function(){};
+
+// function(name, age){
+//     Person.call(this, name, age);
+// };
 Teacher.prototype = Object.create(Person.prototype);
 Teacher.prototype.teach = function(student){
     return `${this.name} is now teaching ${student}`;
 }
 
 console.log("Creating teacher with Name: Carl Sagan, Age: 70");
-const teacher = new Teacher("Carl Sagan", "70");
+const teacher = new Teacher();
+teacher.initialize("Carl Sagan", "70");
 console.log("Adding student for him to teach. Student name: Prabesh K C");
 console.log(teacher.teach("Prabesh K C"));
