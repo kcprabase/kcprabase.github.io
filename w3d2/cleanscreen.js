91c8bfda93a3e4266940844746ad8247141328ed
Prabesh KC <kcprabase@gmail.com>
2022-10-18 20:30:00 -0500

    updated styles for circle

--- w3d2/cleanscreen.js
+++ w3d2/cleanscreen.js
@@ -1,18 +1,49 @@
+let params = {};
 $(() => {
-    $("div").click((e) => {
-        e.currentTarget.remove();
-        // console.log(e);
-        // console.log($(e));
+    $('#startBtn').click(e => {
+        params = getFormData();
+        addCircle();
+        animate();
+        addClickEvent();
     });
-    setInterval(() => {
-        $("div").css("width", (idx, old) => {
-            return parseInt(old) + 10 + "px";
-        });
-        $("div").css("height", (idx, old) => {
-            return parseInt(old) + 10 + "px";
-        });
-        $("div").css("borderRadius", (idx, old) => {
-            return parseInt(old) + 5 + "px";
-        });
-    }, 250);
 });
+
+function getFormData() {
+    return $('#form').serializeArray()
+        .reduce(function (json, { name, value }) {
+            json[name] = value;
+            return json;
+        }, {});
+}
+
+function addCircle() {
+    $('.circle-container').append($('<div>', {
+        'class': 'circle',
+        css: {
+            'width': params.width,
+            'height': params.width,
+            'border-radius': params.width / 2
+        }
+    }));
+}
+
+function animate() {
+    setInterval(() => {
+        $(".circle").css("width", (idx, old) => {
+            var x = parseInt(old) + parseInt(params.growthAmount) + "px";
+            return x;
+        });
+        $(".circle").css("height", (idx, old) => {
+            return parseInt(old) + parseInt(params.growthAmount) + "px";
+        });
+        $(".circle").css("border-radius", (idx, old) => {
+            return parseInt(old) + parseInt(params.growthAmount) / 2 + "px";
+        });
+    }, params.growthRate);
+}
+
+function addClickEvent() {
+    $(".circle").click((e) => {
+        e.currentTarget.remove();
+    });
+}
